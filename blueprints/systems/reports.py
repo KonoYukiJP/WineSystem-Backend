3717d62df9647ffaeba37a00f62f87cdd01fa7cd
河野優輝 <koyu@u-kiMacBook-Air.local>
2025-05-12 16:30:01 +0900

    Report List

--- blueprints/systems/reports.py
+++ blueprints/systems/reports.py
@@ -18,37 +18,21 @@ def fetch_table(query, params = ()):
 def fetch_reports_in_system(system_id):
     query = '''
         SELECT
-            report.id, report.date, 
-            user.id AS user_id,
-            user.name AS username,
-            work.id AS work_id,
-            work.name AS work_name,
-            operation.id AS operation_id,
-            operation.name AS operation_name,
-            report.kind_id AS kind_id,
-            feature.id AS feature_id,
-            feature.name AS feature_name,
-            report.value,
-            feature.unit,
-            report.note
-        FROM reports report
-        JOIN users user ON report.user_id = user.id
-        JOIN works work ON report.work_id = work.id
-        JOIN operations operation ON report.operation_id = operation.id
-        LEFT JOIN features feature ON report.feature_id = feature.id
+            id,
+            date, 
+            user_id,
+            work_id,
+            operation_id,
+            kind_id,
+            feature_id,
+            value,
+            note
+        FROM reports
         WHERE report.system_id = %s
     '''
     reports = fetch_table(query, (system_id, ))
     for report in reports:
         report['date'] = report['date'].strftime('%Y-%m-%dT%H:%M:%SZ')
-        if report['work_id'] == 1:
-            query = 'SELECT name FROM materials WHERE id = %s'
-            materials = fetch_table(query, (report['kind_id'], ))
-            report['kind_name'] = materials[0]['name']
-        else:
-            query = 'SELECT name FROM tanks WHERE id = %s'
-            tanks = fetch_table(query, (report['kind_id'], ))
-            report['kind_name'] = tanks[0]['name']
     return jsonify(reports)
 
 @systems_bp.route('/<int:system_id>/reports', methods = ['POST'])
