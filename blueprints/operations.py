283794e858165e9b8204c8b806c4f75d43cbbf13
河野優輝 <koyu@u-kiMacBook-Air.local>
2025-05-13 17:27:50 +0900

    fixed operation structure

--- blueprints/operations.py
+++ blueprints/operations.py
@@ -15,5 +15,29 @@ def fetch_table(query, params = ()):
 
 @operations_bp.route('', methods = ['GET'])
 def fetch_operations():
+    connection = connect()
+    cursor = connection.cursor(dictionary = True)
+    
     query = 'SELECT id, name, target_type FROM operations'
-    return fetch_table(query, ())
+    cursor.execute(query)
+    operations = cursor.fetchall()
+    
+    for operation in operations:
+        query = '''
+            SELECT feature_id
+            FROM operation_features
+            WHERE operation_id = %s
+        '''
+        cursor.execute(query, (operation['id'], ))
+        feature_ids = cursor.fetchall()
+        
+        operation['feature_ids'] = []
+        for feature_id in feature_ids:
+            operation['feature_ids'].append(feature_id['feature_id'])
+        
+    print(operations)
+    
+    cursor.close()
+    connection.close()
+    
+    return operations
